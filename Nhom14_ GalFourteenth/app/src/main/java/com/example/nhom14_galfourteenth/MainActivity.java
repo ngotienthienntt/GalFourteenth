17bef6d78e279a57cfb72980ee77e22e001a647c
Tran Van Thuong <vanthuong0416@gmail.com>
2021-12-23 23:56:59 +0700

    remove title app

--- Nhom14_ GalFourteenth/app/src/main/java/com/example/nhom14_galfourteenth/MainActivity.java
+++ Nhom14_ GalFourteenth/app/src/main/java/com/example/nhom14_galfourteenth/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
         ft = getSupportFragmentManager().beginTransaction();
         fragmentTop = FragmentTop.newInstance("menus");
         ft.replace(R.id.main_top, fragmentTop);
