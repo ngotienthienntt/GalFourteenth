9515140511ad4817869b0d58d2e37aff13348a8c
DangKhoa <39049676+DangKhoa1999@users.noreply.github.com>
2022-01-03 21:14:12 +0700

    get image path in gallery phone

--- Nhom14_ GalFourteenth/app/src/main/java/com/example/nhom14_galfourteenth/FragmentMiddle.java
+++ Nhom14_ GalFourteenth/app/src/main/java/com/example/nhom14_galfourteenth/FragmentMiddle.java
@@ -1,7 +1,13 @@
 package com.example.nhom14_galfourteenth;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +16,14 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
+import androidx.loader.content.CursorLoader;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class FragmentMiddle extends Fragment implements FragmentCallbacks {
     MainActivity main;
@@ -45,6 +56,44 @@ public class FragmentMiddle extends Fragment implements FragmentCallbacks {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         LinearLayout layout_middle = (LinearLayout) inflater.inflate(R.layout.layout_middle, null);
         gridListImage = (GridView) layout_middle.findViewById(R.id.GridImages);
+        gridListImage.setAdapter(new MyImageAdapter(context, thumbnails));
+        int column_index_data, column_index_date;
+        String pathImage;
+        String[] projection = {
+                MediaStore.Files.FileColumns._ID,
+                MediaStore.Files.FileColumns.DATA,
+                MediaStore.Files.FileColumns.DATE_ADDED,
+                MediaStore.Files.FileColumns.MEDIA_TYPE,
+                MediaStore.Files.FileColumns.MIME_TYPE,
+                MediaStore.Files.FileColumns.TITLE
+        };
+
+// Return only video and image metadata.
+        String selection = MediaStore.Files.FileColumns.MEDIA_TYPE + "="
+                + MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE
+                + " OR "
+                + MediaStore.Files.FileColumns.MEDIA_TYPE + "="
+                + MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
+
+        Uri queryUri = MediaStore.Files.getContentUri("external");
+        CursorLoader cursorLoader = new CursorLoader(context,
+                queryUri,
+                projection,
+                selection,
+                null, // Selection args (none).
+                MediaStore.Files.FileColumns.DATE_ADDED + " DESC" // Sort order.
+        );
+
+        Cursor cursor = cursorLoader.loadInBackground();
+        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        column_index_date=cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DATE_ADDED);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        while (cursor.moveToNext()) {
+            pathImage = cursor.getString(column_index_data);
+            Log.i("TESTLOG", pathImage);
+
+        }
         gridListImage.setAdapter(new MyImageAdapter(context, thumbnails));
 //        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 //            @Override
--- Nhom14_ GalFourteenth/app/src/main/java/com/example/nhom14_galfourteenth/MainActivity.java
+++ Nhom14_ GalFourteenth/app/src/main/java/com/example/nhom14_galfourteenth/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.nhom14_galfourteenth;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +22,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(
+                    new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, 1
+            );
+        }
+
         getSupportActionBar().hide();
+
         ft = getSupportFragmentManager().beginTransaction();
         fragmentTop = FragmentTop.newInstance("menus");
         fragmentBottom = FragmentBottom.newInstance("menus");
